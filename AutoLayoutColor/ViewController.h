456cfa3cfe3e2d0c7e2f507238ee74e096debebe
Gabriel Targon <gabrieltargon@gmail.com>
2013-12-02 10:43:45 -0200

    Initial Commit

---
+++ AutoLayoutColor/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AutoLayoutColor
+//
+//  Created by Gabriel Targon on 12/2/13.
+//  Copyright (c) 2013 BEPID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AutoLayoutColor/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AutoLayoutColor
+//
+//  Created by Gabriel Targon on 12/2/13.
+//  Copyright (c) 2013 BEPID. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
